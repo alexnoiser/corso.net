e1b4e3f720b2775e65b3d85a65ee8a2d01dd371f
allievo04 ires <allievo04.ires@outlook.it>
2017-10-12 12:59:58 +0200

    con mia modificaAlex

--- Corso2017/SmartHome/Program.cs
+++ Corso2017/SmartHome/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,13 +12,22 @@ namespace SmartHome
         static void Main(string[] args)
         {
             Lamp salonLamp = new Lamp("Salon");
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                createMenu();
+                addLamp();
+
+            }
+
             //salon.Room = "Salon";
             //string status = salon.IsOn ? "on" : "off";
 
             WriteStatus(salonLamp);
             salonLamp.TurnOn();
             WriteStatus(salonLamp);
-        
+
             Lamp kitchenLamp = new Lamp("Kitchen");
             WriteStatus(kitchenLamp);
 
@@ -30,9 +40,39 @@ namespace SmartHome
             Console.ReadLine();
         }
 
+        private static void addLamp()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void createMenu()
+        {
+            Console.WriteLine("Menu configurazione centralina SmartHome");
+            Console.WriteLine("[1] Aggiungi Lampadina");
+            Console.WriteLine("[2] Togli Lampadina");
+            Console.WriteLine("[3] Stampa il numero di Lampadine");
+            Console.WriteLine("[4] Esci dal programma");
+            Console.ReadLine();
+
+        }
+
         private static void WriteStatus(Lamp lamp)
         {
             Console.WriteLine($"{ lamp.Room } lamp is { lamp.Status }");
         }
-    }
+
+        private static int GetMenuNumber(input, int selection, int addLamp)
+        {
+            ArrayList lampNumber = new ArrayList();
+
+            if (input == "1")
+                selection = lampNumber.Add(addLamp);
+                return selection;
+        }
+       
+
+
+    }    
+
+    
 }
